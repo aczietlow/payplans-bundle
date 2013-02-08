37a69fbafee7008addc6b3e1f66ff3d5fed6eb38
Chris Zietlow <aczietlow@spyderbytedesign.com>
2013-02-08 15:36:35 -0500

    caputured array of inputs from dynamic input fields

--- bundle/app/bundle/bundle.js
+++ bundle/app/bundle/bundle.js
@@ -8,14 +8,13 @@
 	}
 	
 	payplans.apps.bundle = {
-		calculatePricing : function(invoiceId, familyChildren, familyAdult) {
+		calculatePricing : function(invoiceId, familyMembers) {
 
 			var url = "index.php?option=com_payplans&view=invoice&task=trigger&event=onPayplansInvoiceUpdatePricing";
 			var args = {
 				'event_args' : {
 					'invoiceId' : invoiceId,
-					'familyChildren' : 1,
-					'familyAdult'	:4,
+					'familyChildren' : familyMembers,
 				}
 			};
 			//alert(invoiceId+'|'+url);
@@ -26,31 +25,62 @@
 
 	payplans.jQuery('document').ready(function() {
 		
-		var i = payplans.jQuery('#.pp-app-bundle-inputs input').size() + 1;
+		var i = payplans.jQuery('#.pp-app-bundle-inputs input').size();
 		
 		payplans.jQuery('#pp-custom-calculate').click(function () {
 			var invoiceId = payplans.jQuery('input[name="invoiceId"]').val();
-			alert($(":input[name='family']").val().length);
-			
-			payplans.apps.bundle.calculatePricing(invoiceId);
+			var familyMembers = i - 1;
+			payplans.apps.bundle.calculatePricing(invoiceId, familyMembers );
 		});
 		
+		//add 1 add family field set
 		payplans.jQuery('#pp-custom-addFamily').click(function() {
-			payplans.jQuery('<div><input type="text" class="field" name="dynamic[]" value="' + i + '" /></div>').fadeIn('slow').appendTo('.pp-app-bundle-inputs');
+			payplans.jQuery('<div class="field">' +
+					'<label>Family Member Name</label><input type="text" class="fieldFamilyName" name="dynamic[]" value="' + i + '" /><br />'+
+					'<label>Sex| </label>' +
+					'<label>Male</label><input type="radio" name="dynamic[]" value="M" />'+
+					'<label>Female</label><input type="radio" name="dynamic[]" value="F" /><br />'+
+					'<label>Date of Birth</label><input type="text"  name="dynamic[]" value="' + i + '" /><br />'+
+					'<label>Under 18 | </label>' +
+					'<label>Yes</label><input type="radio" name="dynamic[]" value="True" />'+
+					'<label>No</label><input type="radio" name="dynamic[]" value="False" /><br /><hr />'+
+					'</div>').fadeIn('slow').appendTo('.pp-app-bundle-inputs');
 	        i++;
 		});
+		
+		//remove 1 add family field set
 		payplans.jQuery('#pp-custom-removeFamily').click(function() {
 			if(i > 1) {
 		        $('.field:last').remove();
 		        i--;
 		    }
 		});
+		
+		//reset add family fields
 		payplans.jQuery('#pp-custom-resetFamily').click(function() {
 			while(i > 2) {
 		        $('.field:last').remove();
 		        i--;
 		    }
 		});
+		
+		//sibmit function
+		payplans.jQuery('#payplans-order-confirm').click(function(){
+            
+		    var answers = [];
+		    payplans.jQuery.each(payplans.jQuery('.fieldFamilyName'), function() {
+		        answers.push(payplans.jQuery(this).val());
+		    });
+		     
+		    if(answers.length == 0) {
+		        answers = "none";
+		    }  
+		 
+		    alert(answers);
+		     
+		    return false;
+		                                 
+		    });
 	});
 	// ENDING :
 	// Scoping code for easy and non-conflicting access to $.
